4a1d3d6e7cb387ab40f1c543a442c3cc24a7b229
Gabriel "fr4ct1ons" Lucena <gabriellucena23@gmail.com>
2020-06-21 19:22:01 -0300

    Added frog behaviour

--- Bat Simulator/Assets/scripts/Frog/FrogBehaviour.cs
+++ Bat Simulator/Assets/scripts/Frog/FrogBehaviour.cs
@@ -2,15 +2,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
+[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class FrogBehaviour : MonoBehaviour
 {
     [SerializeField] private float minJumpForce = 100.0f, maxJumpForce = 100.0f;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float timeBetweenJumps;
+    [SerializeField] private GroundChecker2D groundChecker;
+    [SerializeField] private Animator anim;
+    [SerializeField] private float damage;
+
+    private float currentJumpTime;
 
     private void Awake()
     {
-        
+        if (!anim)
+            anim = GetComponent<Animator>();
+        if (!rb)
+            rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (groundChecker.isGrounded)
+        {
+            currentJumpTime += Time.deltaTime;
+            if (currentJumpTime >= timeBetweenJumps)
+            {
+                rb.AddForce(new Vector2(0.0f, Random.Range(minJumpForce, maxJumpForce)));
+                currentJumpTime = 0.0f;
+                anim.SetTrigger("Jump");
+                anim.SetBool("IsGrounded", false);
+                Debug.Log("Jump");
+            }
+        }
+    }
+
+    public void IdleAnimation()
+    {
+        anim.SetBool("IsGrounded", true);
+    }
+    
+    public void JumpAnimation()
+    {
+        anim.SetTrigger("Jump");
+    }
+
+    public void DeathAnimation()
+    {
+        anim.SetTrigger("Death");
+    }
+
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+    public void HitAnimation()
+    {
+        anim.SetTrigger("Hit");
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.TryGetComponent<Stats>(out Stats otherStats))
+        {
+            otherStats.SendDamage(damage);
+        }
     }
 }
--- Bat Simulator/Assets/scripts/GroundChecker2D.cs
+++ Bat Simulator/Assets/scripts/GroundChecker2D.cs
@@ -2,18 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Collider2D))]
 public class GroundChecker2D : MonoBehaviour
 {
     public bool isGrounded;
+
+    public UnityEvent onBecomeGrounded, onBecomeUngrouded;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        {
-            isGrounded = true;
-        }
+        isGrounded = true;
+        onBecomeGrounded.Invoke();
     }
 
     private void OnTriggerStay2D(Collider2D other)
@@ -23,9 +24,7 @@ public class GroundChecker2D : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        {
-            isGrounded = false;
-        }
+        isGrounded = false;
+        onBecomeUngrouded.Invoke();
     }
 }
